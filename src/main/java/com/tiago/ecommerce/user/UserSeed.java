638b209dc4cc0ebc03f7b342dcca194db547943d
Tiago Jacinto <tiagojacintodev@gmail.com>
2023-04-07 13:56:53 +0100

    Implement users seed class

---
+++ src/main/java/com/tiago/ecommerce/user/UserSeed.java
@@ -0,0 +1,55 @@
+package com.tiago.ecommerce.user;
+
+import com.tiago.ecommerce.role.Role;
+import com.tiago.ecommerce.role.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.management.relation.RoleNotFoundException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE + 1)
+public class UserSeed implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        seedUsers();
+    }
+
+    private void seedUsers() throws RoleNotFoundException {
+        User user = userRepository.findByUsernameIgnoreCase("user").orElse(null);
+        User admin = userRepository.findByUsernameIgnoreCase("admin").orElse(null);
+
+        if(user == null) {
+            Role user_role = roleRepository.findByNameIgnoreCase("user")
+                    .orElseThrow(RoleNotFoundException::new);
+
+            User newUser = new User();
+            newUser.setUsername("user");
+            newUser.setPassword("user");
+            newUser.getRoles().add(user_role);
+
+            userRepository.save(newUser);
+        }
+
+        if(admin == null) {
+            Role admin_role = roleRepository.findByNameIgnoreCase("admin")
+                    .orElseThrow(RoleNotFoundException::new);
+
+            User newAdmin = new User();
+            newAdmin.setUsername("admin");
+            newAdmin.setPassword("admin");
+            newAdmin.getRoles().add(admin_role);
+
+            userRepository.save(newAdmin);
+        }
+    }
+}
