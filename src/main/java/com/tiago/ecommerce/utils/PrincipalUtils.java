b9bb69b1c23faf4be18413c3148af7af4285b23a
Tiago Jacinto <tiagojacintodev@gmail.com>
2023-04-08 12:36:14 +0100

    Improve code readability

--- src/main/java/com/tiago/ecommerce/utils/PrincipalUtils.java
+++ src/main/java/com/tiago/ecommerce/utils/PrincipalUtils.java
@@ -1,28 +1,26 @@
 package com.tiago.ecommerce.utils;
 
-import com.tiago.ecommerce.user.User;
-import com.tiago.ecommerce.user.UserRepository;
 import lombok.experimental.UtilityClass;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.util.UUID;
 
 @UtilityClass
 public class PrincipalUtils {
+    private final Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
     public boolean isAdmin() {
-        return SecurityContextHolder.getContext().getAuthentication()
+        return auth
                 .getAuthorities().stream()
                 .anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"));
     }
 
     public boolean isUser() {
-        return SecurityContextHolder.getContext().getAuthentication()
+        return auth
                 .getAuthorities().stream()
                 .anyMatch(auth -> auth.getAuthority().equals("ROLE_USER"));
     }
 
     public String getName() {
-        return SecurityContextHolder.getContext().getAuthentication().getName();
+        return auth.getName();
     }
 }
