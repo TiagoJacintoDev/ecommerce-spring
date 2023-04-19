0f6d443e5904313ce175aaf7991a35bad0015b7b
TiagoJacintoDev <tiagojacintodev@gmail.com>
2023-04-19 16:43:58 +0100

    Chore: small changes

--- src/main/java/com/tiago/ecommerce/product/ProductService.java
+++ src/main/java/com/tiago/ecommerce/product/ProductService.java
@@ -1,6 +1,7 @@
 package com.tiago.ecommerce.product;
 
 import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.UUID;
 
+@AllArgsConstructor
 @Service
 public class ProductService {
     @Autowired
@@ -30,7 +32,7 @@ public class ProductService {
         }
 
         productRepository.delete(product);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok("Product deleted successfully");
     }
 
     public ResponseEntity<Object> getById(UUID id) {
